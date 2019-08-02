4133ae22782721aa1d5701625242762e8febe756
anchen <anthony.chen@mdacorporation.com>
2019-08-02 10:20:48 -0700

    got bare skeleton of side bar

--- src/widgets/side-bar/side-bar.js
+++ src/widgets/side-bar/side-bar.js
@@ -1,12 +1,64 @@
 import React from 'react';
-class SideBar extends React.Component {
+import { Button, Header, Icon, Image, Menu, Segment, Sidebar } from 'semantic-ui-react'
+// https://alligator.io/react/react-burger-menu-sidebar/
+// https://react.semantic-ui.com/usage
+export default class SideBar extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // TODO move visible state to be a part of the prop
+    this.state = {
+      visible: false
+    };
+  }
+
+  toggleVisibility () {
+    this.setState({
+      ...this.state,
+      visible: !this.state.visible
+    });
+  }
+
   render() {
     return (
-      <div>
-        I am a side bar
-      </div>
+      <>
+      <Button 
+      onClick={ () => this.toggleVisibility() }>
+        Toggle sidebar visibililty
+      </Button>
+      <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={() => this.toggleVisibility()}
+            vertical
+            visible={this.state.visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>Application Content</Header>
+              <Image src='https://react.semantic-ui.com/images/wireframe/paragraph.png' />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </>
     );
   }
 }
-
-export default SideBar;
