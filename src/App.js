f14a942a78a275f7640709f088be300aa2839c98
anchen <anthony.chen@mdacorporation.com>
2019-08-04 15:36:26 -0700

    got in partial footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Icon } from 'semantic-ui-react';
+import { Icon, Menu } from 'semantic-ui-react';
 import './App.css';
 import SideBar from './widgets/side-bar/side-bar.js';
+import { Page } from './models/page';
 
 export default class App extends React.Component {
 
   constructor(props) {
     super(props);
+    const menuItemsIds = Object.keys(Page).map(key => Page[key]);
     this.state = {
       selectedMenuItem: undefined,
       sideBarVisible: false,
-      menuItemIds: ["1", "2", "3"]
+      menuItemIds: menuItemsIds
     };
   }
 
@@ -30,6 +32,29 @@ export default class App extends React.Component {
           menuItemIds = {this.state.menuItemIds}
           visibilityChanged = { (visibile) => this.setVisibility(visibile) } 
           menuItemClicked = { (menuItemId) => this.onMenuItemClicked(menuItemId) } />
+
+        <Menu fixed='bottom' inverted widths={4}>
+          {/* <Menu.Item onClick={ () => this.toggleSidebarVisibility() }>
+            <Icon 
+                name='bars'
+                size={'large'}/>
+          </Menu.Item> */}
+          <Menu.Item>
+            Anthony Chen
+          </Menu.Item>
+          <Menu.Item>
+            <Icon name='mail'/>
+            achen7x7@gmail.com
+          </Menu.Item>
+          <Menu.Item href='https://github.com/AnthonyChen7' target='_blank'>
+            <Icon name='github' />
+            My Github
+          </Menu.Item>
+          <Menu.Item href='https://www.linkedin.com/in/anthony-chen-94a03a7b/' target='_blank' >
+            <Icon name='linkedin' />
+            My LinkedIn
+          </Menu.Item>
+        </Menu>
       </React.Fragment>
     );
   }
---
+++ src/models/page.js
@@ -0,0 +1,5 @@
+export const Page = {
+  ABOUT_ME : 'About Me',
+  WORK_EXP : 'Work Experience',
+  BADMINTON : 'Badminton'
+}
