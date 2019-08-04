957048db9cf3de24fe2941967bf9c98a85319a1b
anchen <anthony.chen@mdacorporation.com>
2019-08-04 16:09:45 -0700

    can copy email and show feedbabck

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Icon, Menu } from 'semantic-ui-react';
+import { Icon, Menu, Popup } from 'semantic-ui-react';
 import './App.css';
 import SideBar from './widgets/side-bar/side-bar.js';
 import { Page } from './models/page';
 
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+const timeoutLength = 1500;
+
 export default class App extends React.Component {
+
+  emailMenuItemRef = React.createRef();
 
   constructor(props) {
     super(props);
@@ -12,40 +18,63 @@ export default class App extends React.Component {
     this.state = {
       selectedMenuItem: undefined,
       sideBarVisible: false,
-      menuItemIds: menuItemsIds
+      menuItemIds: menuItemsIds,
+      emailCopied: false
     };
+  }
+
+  onCopy(text, result) {
+    if (result) {
+      this.setState({
+        emailCopied: true
+      });
+
+      this.timeout = setTimeout(() => {
+        this.setState({ emailCopied: false });
+      }, timeoutLength);
+    }
+  }
+
+  handlePopUpClose() {
+    this.setState({ emailCopied: false });
+    clearTimeout(this.timeout);
   }
 
   render() {
     // https://reactjs.org/docs/fragments.html
     return (
       <React.Fragment>
-        <div
-          className="menu-container"
-          onClick={ () => this.toggleSidebarVisibility() }>
-          <Icon 
-            name='bars'
-            size={'large'}/>
-        </div>
+        <Popup
+          context={this.emailMenuItemRef}
+          content='Email Successfully Copied'
+          position='top center'
+          open={this.state.emailCopied}
+          onClose={() => this.handlePopUpClose()}
+        />
         <SideBar 
           visible= {this.state.sideBarVisible}
           menuItemIds = {this.state.menuItemIds}
           visibilityChanged = { (visibile) => this.setVisibility(visibile) } 
           menuItemClicked = { (menuItemId) => this.onMenuItemClicked(menuItemId) } />
 
-        <Menu fixed='bottom' inverted widths={4}>
-          {/* <Menu.Item onClick={ () => this.toggleSidebarVisibility() }>
+        <Menu fixed='top' inverted widths={5}>
+          <Menu.Item onClick={ () => this.toggleSidebarVisibility() }>
             <Icon 
                 name='bars'
                 size={'large'}/>
-          </Menu.Item> */}
+          </Menu.Item>
           <Menu.Item>
             Anthony Chen
           </Menu.Item>
-          <Menu.Item>
-            <Icon name='mail'/>
-            achen7x7@gmail.com
-          </Menu.Item>
+          <CopyToClipboard
+            onCopy={(text, result) => this.onCopy(text, result)}
+            text='achen7x7@gmail.com'>
+              <Menu.Item>
+                <Icon name='mail'/>
+                <span ref={this.emailMenuItemRef}>achen7x7@gmail.com</span>
+              </Menu.Item>
+          </CopyToClipboard>
+          
           <Menu.Item href='https://github.com/AnthonyChen7' target='_blank'>
             <Icon name='github' />
             My Github
